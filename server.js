b3e4100b7cc1c46b8b5f9193033158ecf768ae12
Hugo Fooy <hugo.fooy@outlook.com>
2019-08-23 12:55:51 +0300

    Update copyright and license info

--- server.js
+++ server.js
@@ -1,36 +1,53 @@
 /*
-* Copyright (c) TUT Tampere University of Technology 2014-2015.
-* All rights reserved.
-* This software has been developed in Tekes-TIVIT project Need-for-Speed.
-* All rule set in consortium agreement of Need-for-Speed project apply.
-*
-* Main authors: Antti Luoto, Anna-Liisa Mattila, Henri Terho
+* Copyright 2018-2019 Tampere University
+* 
+* Main authors: Anna-Liisa Mattila, Henri Terho, Antti Luoto, Hugo Fooy
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in 
+* the Software without restriction, including without limitation the rights to 
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so, 
+* subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 //require separate db module
 //var db = require()
 
 var express = require('express');
-var app     = express();
+var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
 var config = require('config');
- 
+
 
 //config
 app.use(bodyParser.urlencoded({
-    extended: true,
-    limit: '50mb'
-  }));
+  extended: true,
+  limit: '50mb'
+}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
 app.use(express.static(__dirname + '/frontend'));
 app.use(express.static(__dirname + '/data-collector'));
 
-app.use(errorHandler({dumpExceptions: true, showStack: true}));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
 
 //DATABASE 
 var db = require("./backend/database/database.js");
